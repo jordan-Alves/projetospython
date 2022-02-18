55f3613cba0b56d2b7d8f1d21fc69c678090c200
Jordan Alves <jordanmonteiro141@gmail.com>
2022-02-17 21:30:20 -0300

    testes apagados

--- ativiade_f.2.py
+++
@@ -1,7 +0,0 @@
-import copy
-list = []
-lista1 = ['idade','nome','peso']
-lista2 = ['jhon',40,1.8]
-
-list.append(lista2,lista1)
-print(list)
--- main.py
+++
@@ -1,5 +0,0 @@
-import os
-
-from  modulos.calculos import soma
-
-a = int(input)
